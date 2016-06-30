d9eec16d692de42ad31e61650b246e6bad33a3f6
lee <lee@clearlovedeMacBook-Pro.local>
2016-06-30 17:30:03 +0800

    first commit

---
+++ OOSegmentViewController/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  OOSegmentViewController
+//
+//  Created by lee on 16/6/28.
+//  Copyright © 2016年 clearlove. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: OOSegmentViewController {
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        titles = [
+            "One",
+            "Two",
+            "HHHHHH"
+        ]
+        
+        controllers = [
+            createController(),
+            createController(),
+            createController()
+        ]
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(action))
+        
+    }
+    
+    func action() {
+        self.pageIndex = 2
+    }
+    
+    func createController() -> UIViewController {
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor(red: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), green: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), blue: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), alpha: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)))
+        return vc
+    }
+
+}
--- OOSegmentViewController/ViewController.swift
+++ OOSegmentViewController/ViewController.swift
@@ -13,8 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(frame: CGRectMake(0,100,CGRectGetWidth(self.view.frame),30))
+        button.setTitle("Click ME", forState: .Normal)
+        button.setTitleColor(UIColor.redColor(), forState: .Normal)
+        button.addTarget(self, action: #selector(clickAction), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
     }
 
+    func clickAction() {
+        let vc = OOSegmentViewController()
+        vc.titles = [
+            "WorkKKKK",
+            "Job",
+            "MaskLLLLL",
+            "Life",
+            "Live",
+            "MissWWWWW",
+            "GOHomeHHHH",
+            "TESTSSSSSS"
+        ]
+        vc.controllers = [
+            createController(),
+            createController(),
+            createController(),
+            createController(),
+            createController(),
+            createController(),
+            createController(),
+            createController()
+        ]
+        vc.cursorColor = UIColor.blueColor()
+        vc.titleSelectedColor = UIColor.blueColor()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    func createController() -> UIViewController {
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor(red: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), green: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), blue: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)), alpha: CGFloat(CGFloat(random())/CGFloat(RAND_MAX)))
+        return vc
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
